5753e40ed6ef9259435a3e88fe63382b907e7ba7
dineshmoorthy <contactdineshmoorthy@gmail.com>
2019-07-19 12:21:00 +0530

    Rename samplestream.java to samplestreamjavaeight.java
    sample code

---
+++ src/main/java/com/example/samplestreamjavaeight.java
@@ -0,0 +1,54 @@
+// Below code is remove objects from parentobject after iterating based on condition
+public List<Group> getOtherGroups(String userid) {
+			// method 1
+			List<Group> allgroupList = groupRepository.findAll();
+			List<String> usergroupList = new ArrayList<>();
+			
+			usergroupList = userRepository.findGroupsById(userid);
+			if (CollectionUtils.isNotEmpty(usergroupList)) {
+				for (String usergroup : usergroupList) {
+					Iterator<Group> iterator = allgroupList.iterator();
+					while (iterator.hasNext()) {
+						Group group = iterator.next();
+						if (group.getId().equals(usergroup)) {
+							iterator.remove();
+						}
+
+					}
+				}
+			}
+			return allgroupList;
+		
+		}*/
+		
+	public List<Group> tryjavaeight(String userid) {
+  // method 2
+		List<Group> allgroupList = groupRepository.findAll();
+		final List<String> usergroupList = userRepository.findGroupsById(userid);
+		List<Group> othergroupList = new ArrayList<Group>();
+		allgroupList.forEach(g -> {
+			usergroupList.forEach(h -> {
+				if (h.equals(g.getId())) {
+					othergroupList.add(g);
+				}
+			});
+		});
+
+		allgroupList.removeAll(othergroupList);
+		return allgroupList;
+	}
+		
+	
+	@Override
+	public List<Group> getOtherGroups(String userid) {
+  // method 3
+		//public List<Group> filterjavaeight(String userid) {
+
+		List<Group> allgroupList = groupRepository.findAll();
+		final List<String> usergroupList = userRepository.findGroupsById(userid);
+		allgroupList.removeIf((Group pp) -> {
+			return usergroupList.stream().anyMatch(x -> (pp.getId().equals(x)));
+		});
+		return allgroupList;
+
+	}
